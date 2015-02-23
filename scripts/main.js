89a312bea1f088a4aaa3e6dc8a9a1812f6dc261d
Eric Lehmann <elehmann@26l2233a3-09.thecementworks.com>
2015-02-23 17:08:44 -0500

    worked on analyzer

--- scripts/main.js
+++ scripts/main.js
@@ -47,6 +47,8 @@ $(function() {
     //         showAlert("password must be > 6 characters");
     //     }
     // }
+
+
     
 })
 
@@ -158,4 +160,38 @@ function setHours(n) {
     // For Mozilla browser: e.g. Firefox
     $("#hours").css({ '-moz-transform': 'rotate(' + rotateBy + 'deg)'});
 }
+
+
+
+
+
+    // SYSTEM ANALIZER
+
+    // This script sets OSName variable as follows:
+    // "Windows"    for all versions of Windows
+    // "MacOS"      for all versions of Macintosh OS
+    // "Linux"      for all versions of Linux
+    // "UNIX"       for all other UNIX flavors 
+    // "Unknown OS" indicates failure to detect the OS
+
+    var OSName="Unknown OS";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+    function osTest() {
+        $("#OSspan").html(OSName);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
